2a286441bb0f57395466e0b73af15bd4d972a690
lilei2603 <240924085@qq.com>
2023-04-27 17:40:26 +0800

    chore: rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/resume',
+	site: 'https://leiss.me',
+	base: 'resume',
 	integrations: [mdx(), sitemap()],
 });
